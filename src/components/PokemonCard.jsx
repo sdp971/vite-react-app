98d1d463fc7c2002021d95e0d9db66da201a1c8a
Séverine Dorimond Pereira <doris190485@hotmail.com>
2024-03-28 21:24:16 +0100

    PokemonCard created

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,19 @@
+
+
+function PokemonCard()
+{
+
+  return (
+    <div>
+      <figure>
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+          alt='le Pokemon Bulbasaur'
+        />
+      </figure>
+    </div>
+  );
+}
+
+
+export default PokemonCard;
