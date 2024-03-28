2e5b686dc5faf9ad08672acd44387dbe9d301cea
Séverine Dorimond Pereira <doris190485@hotmail.com>
2024-03-28 23:00:42 +0100

    React Basics 04 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: 'bulbasaur',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+];
 
 function App() {
- 
-
   return (
-   
-      <div>
-      <PokemonCard/>
-        
-    
-      </div>
-     
-    
-  )
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,12 @@
 
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard()
+
+function PokemonCard({pokemon})
 {
-  
-  const pokemon = {
-    name: 'bulbasaur',
-    imgSrc:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-  };
 
+
+ 
   
   return (
     <div>
