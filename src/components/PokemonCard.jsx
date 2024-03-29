3edc5dbe387748893c735dc7cc54bf484b92d61d
Séverine Dorimond Pereira <doris190485@hotmail.com>
2024-03-29 09:56:50 +0100

    React basis 05

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,25 @@
+import PropTypes from 'prop-types';
 
-
-
-
-function PokemonCard({pokemon})
-{
-
-
- 
-  
+function PokemonCard({ pokemon }) {
   return (
     <div>
       <figure>
-       {pokemon.imgSrc ?
-          <img
-          src={pokemon.imgSrc}
-          alt={`le Pokemon ${pokemon.name}`}
-        /> : <p>???</p>}
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={`le Pokemon ${pokemon.name}`} />
+        ) : (
+          <p>???</p>
+        )}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
   );
 }
 
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default PokemonCard;
